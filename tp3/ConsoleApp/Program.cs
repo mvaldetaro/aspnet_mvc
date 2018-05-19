4ae4adcde3f85e1a7882f9de27f1751d1d852bda
Magno Valdetaro <mvaldetaro@gmail.com>
2018-05-19 11:40:47 -0300

    ConsoleApp consome api

--- tp3/ConsoleApp/Program.cs
+++ tp3/ConsoleApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,24 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            using (var apiClient = new HttpClient())
+            {
+                var mediaType = new MediaTypeWithQualityHeaderValue("aplication/json");
+
+                apiClient.BaseAddress = new Uri("https://localhost:44352/");
+                apiClient.DefaultRequestHeaders.Accept.Add(mediaType);
+
+                var message = apiClient.GetAsync("api/Amigoes").Result;
+
+                if (message.IsSuccessStatusCode)
+                {
+                    Console.Write(message.Content.ReadAsStringAsync().Result);
+                } else
+                {
+                    Console.Write("Deu xabu!");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
